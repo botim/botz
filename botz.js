931785823db8ce8147d2e86e5486d0faeeb8d8ab
Eran Cohen <ceranco@gmail.com>
2019-02-07 23:41:15 +0200

    accept merge

--- botz.js
+++ botz.js
@@ -1,24 +1,5 @@
-<<<<<<< HEAD
 document.body.style.border = "5px solid blue";
 $(document).ready(function () {
     console.log(getApiGateway().getScore());
 });
-=======
-//const Tweet = require('./tweet.js');
-document.body.style.border = "5px solid red";
-// tweets = HTMLcollection of all tweets in a page.
-var tweets = document.getElementsByClassName('tweet');
-var tweets_arr = Array.prototype.slice.call(tweets);
-var i, tweet, id;
-var user_ids = [];
-console.log("Num Tweets: " + tweets.length);
-for (i = 0; i < tweets.length; i++) {
-    try {
-        tweet = new Tweet(tweets_arr[i]);
-        console.log(tweet.userID);
-    } catch (e) {
-        console.log(e);
-    }
-}
->>>>>>> master
 
