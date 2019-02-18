12430660e30fed2d43e913fb0d8e13c3c1db18ed
Akiva <bloch.akiva@gmail.com>
2019-02-18 21:56:32 +0200

    refactor

--- tweet.js
+++ tweet.js
@@ -1,11 +1,15 @@
 class Tweet {
     constructor(div) {
         if (!div.classList.contains("tweet")) {
-            throw "Div is not a class 'tweet'";
+            throw "Div " + div + " is not a class 'tweet'";
         }
         this.div = div;
         this.userID = div.getAttribute('data-user-id')
     };
+
+    markAsBot() {
+      this.setBorder("solid 4px red");
+    }
 
     setBorder(border) {
         this.div.style.border = border;
