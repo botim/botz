b1a452ce51fc3e682d72dbdcdad4662faf87b109
Yuval Larom <ylarom@gmail.com>
2019-03-09 09:48:04 +0200

    initial integration

--- src/core/api-service.ts
+++ src/core/api-service.ts
@@ -1,6 +1,6 @@
 import { Status } from './symbols';
 
-const apiUrl = 'https://reqres.in/api/user/1';
+const apiUrl = 'http://localhost:8080/bots/';
 
 // TODO: move storage to indexdb?
 export class ApiService {
@@ -8,18 +8,21 @@ export class ApiService {
     window.localStorage.botz = window.localStorage.botz || JSON.stringify({});
   }
 
-  public async checkIfBot(userId: string | number) {
+  public async checkIfBot(userId: string | number, network: string) {
     const cachedValue = this._getFromCache(userId);
+    console.log('_gotCache', userId, cachedValue);
 
     if (cachedValue) {
+      console.log('returning cached data');
       return cachedValue;
     }
+    console.log('fetching data for', userId);
+    const botData = await this._callServer([userId as string], network);
+    console.log('_callServer', userId, botData);
+    //const status = isBot ? Status.BOT : Status.NOT_BOT;
+    this._storeInCache(userId, botData);
 
-    const isBot = await this._callServer(userId as string);
-    const status = isBot ? Status.BOT : Status.NOT_BOT;
-    this._storeInCache(userId, status);
-
-    return status;
+    return botData;
   }
 
   public async report(body: any): Promise<boolean> {
@@ -36,23 +39,24 @@ export class ApiService {
   private _storeInCache(userId: string | number, value: string) {
     const cache = this._getCache();
     cache[userId] = value;
+    console.log('_storeInCache', userId, cache[userId]);
 
     window.localStorage.setItem('botz', JSON.stringify(cache));
   }
 
   private _getFromCache(userId: string | number): string | null {
     const cache = this._getCache();
-
+    console.log('_getFromCache', userId, cache[userId]);
     if (userId in cache) {
       return cache[userId];
     }
   }
 
-  private async _callServer(userId: string): Promise<boolean> {
-    // todo: use the actual response
-    // const response = await fetch(apiUrl);
+  private async _callServer(userIds: string[], network: string): Promise<string> {
+    const idsParam = 'userIds[]=' + userIds.join('&userIds[]=');
+    console.log(apiUrl + 'confirmed?' + idsParam + '&platform=' + network);
+    const response = await fetch(apiUrl + 'confirmed?' + idsParam + '&platform=' + network);
 
-    // return true;
-    return userId.search(/^[sreby]/i) >= 0;
+    return response.text();
   }
 }
