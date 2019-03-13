51fc1dd9840c6bea100e69259c7897ca0808f0b0
eranshmil <eranshmil@users.noreply.github.com>
2019-03-13 22:25:09 +0200

    feat: implement basic cache using indexeddb

--- src/core/consts.ts
+++ src/core/consts.ts
@@ -11,3 +11,5 @@ export const MODAL_CLOSE_CLASS = 'botz-close';
 export const MODAL_REPORTER_KEY_SHOW_CLASS = 'hidden';
 
 export const API_URL = process.env.API_URL || 'http://localhost:8080/bots';
+
+export const CACHE_EXPIRE_MIN = 15;
