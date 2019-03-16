1472181bcacd65166b958b78da97a7327a7569d8
eranshmil <eranshmil@users.noreply.github.com>
2019-03-16 21:48:03 +0200

    feat: enable duplicate reports
    remove reported mode from ui

--- src/core/consts.ts
+++ src/core/consts.ts
@@ -1,5 +1,4 @@
 export const REPORT_BUTTON_CLASS = 'botz-report-button';
-export const REPORTED_BUTTON_CLASS = 'botz-reported-button';
 export const VISITED_CLASS = 'botz-visited';
 export const DETECTED_BOT_CLASS = 'botz-detected';
 
@@ -8,7 +7,7 @@ export const MODAL_WRAPPER_SUBMITTED_CLASS = 'botz-modal-wrapper-submitted';
 export const MODAL_CLASS = 'botz-modal';
 export const MODAL_SUBMIT_CLASS = 'botz-submit';
 export const MODAL_CLOSE_CLASS = 'botz-close';
-export const MODAL_REPORTER_KEY_SHOW_CLASS = 'hidden';
+export const MODAL_HIDE_REPORTER_KEY_CLASS = 'hidden';
 
 // as defined in webpack.config.js
 export const { API_URL } = process.env;
